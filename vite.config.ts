ff5d274545bdd59976dc4f63bc97d3edb9146f5c
jldec <jldec@users.noreply.github.com>
2024-01-24 20:59:55 +0000

    test vite-adapter and paraglide with no project-id

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { paraglide } from '@inlang/paraglide-js-adapter-vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    paraglide({
+      project: './my-project.inlang', //Path to your inlang project
+      outdir: './src/paraglide', //Where you want the generated files to be placed
+    }),
+    sveltekit(),
+  ],
 });
