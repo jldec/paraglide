5368ade05b46b449d4e1609747c8f61ea4d395c0
jldec <jldec@users.noreply.github.com>
2024-01-25 12:01:38 +0000

    match vite.config also

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import ViteRestart from 'vite-plugin-restart';
 import { paraglide } from '@inlang/paraglide-js-adapter-vite';
+import UnoCSS from 'unocss/vite';
 
 export default defineConfig({
   plugins: [
-    paraglide({
-      project: './pursiny-sveltekit.inlang', //Path to your inlang project
-      outdir: './src/paraglide', //Where you want the generated files to be placed
-    }),
+    UnoCSS(),
     sveltekit(),
+    paraglide({
+      project: './pursiny-sveltekit.inlang',
+      outdir: './src/paraglide',
+    }),
+    ViteRestart({ restart: 'mdsvex.config.js' }),
   ],
 });
