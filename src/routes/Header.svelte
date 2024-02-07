5ca36b96062d134d70cdb1011957622f1bf7bc7e
jldec <>
2024-02-07 09:57:59 -0500

    Add untranslated message, try to use paraglide from monorepo

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -27,6 +27,7 @@
 				<a href="/sverdle">Sverdle</a>
 			</li>
 		</ul>
+		[{m.a_message_without_a_translation()}]<br>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
 		</svg>
